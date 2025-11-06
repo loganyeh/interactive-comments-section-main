13b6e99611b1623bbfb6e5e8fe81be990bef1a0e
loganyeh <yehlogan@gmail.com>
2025-11-06 15:53:25 -0500

    feat: tweet layout made

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ let isComment = true;
 // CALLS
 let count = 1;
 const num = 5;
-for(let i = 0; i < num; i++){
-    const post = document.createElement(`div`);
-    post.id = `tweet-${count}`;
-    post.className = `h-1/6 w-5/6 m-3 rounded border-2 border-red-600`;
-    post.textContent = `TWEET`;
-    timeline.appendChild(post);
-    count++;
-};
+// for(let i = 0; i < num; i++){
+//     const post = document.createElement(`div`);
+//     post.id = `tweet-${count}`;
+//     post.className = `h-1/6 w-5/6 grid grid-rows-4 grid-cols-4 m-3 rounded border-2 border-red-600`;
+//     post.textContent = `TWEET`;
+//     timeline.appendChild(post);
+//     count++;
+// };
 
 // SANDBOX
