7ff833fe1ceef6e5f5d856b7e152d36a94be7621
loganyeh <yehlogan@gmail.com>
2025-11-05 20:11:43 -0500

    fix: timeline layout

---
+++ index.js
@@ -0,0 +1,25 @@
+// DOM ELEMENTS
+const container = document.getElementById(`container`);
+const stack = document.getElementById(`stack`);
+const timeline = document.getElementById(`timeline`);
+
+// VARIABLES
+let isTweet = true;
+let isReply = true;
+let isComment = true; 
+
+// FUNCTIONS
+function tweet(){
+    
+}
+function reply(){
+
+}
+function comment(){
+
+}
+
+// CALLS
+
+
+// SANDBOX
