2029ad3c64f5dfc3c8d4cd57bac2a7173bae3ffd
loganyeh <yehlogan@gmail.com>
2025-11-07 15:50:11 -0500

    wip: reply comment section

--- index.js
+++ index.js
@@ -1,98 +1,16 @@
 // ----- DOM ELEMENTS ----- 
 
-    // UPVOTE CONTAINER DOM ELEMENTS
-    const upvoteContainer = document.getElementById("upvote-container");
-    const upvote = document.getElementById("upvote");
-    const totalUpvotes = document.getElementById("total-upvotes");
-    const downvote = document.getElementById("downvote");
-
-// REPLY BUTTON DOM 
-const replyButton = document.getElementById("reply-button");
-
 // VARIABLES
-let isTweet = true;
-let isReply = true;
-let isComment = true; 
-// let currentUpvotes = Number(totalUpvotes.innerText);
-let isClicked = false;
 
 // EVENT LISTENERS
-// upvote.addEventListener("click", () => {
-//     upvoteTick();
-// });
-// downvote.addEventListener("click", () => {
-//     downvoteTick();
-// });
-// replyButton.addEventListener("click", () => {
-//     console.log("reply button clicked");
-// })
 
 // FUNCTIONS
-function upvoteTick(){
-    isClicked = !isClicked;
-    if(isClicked){
-        currentUpvotes++;
-        totalUpvotes.innerHTML = `
-            ${currentUpvotes}
-        `;
-        setTimeout(() => {
-            totalUpvotes.style.color = "green";
-        }, 12);
-        setTimeout(() => {
-            totalUpvotes.style.color = "#1E40AF";
-        }, 1000);
-        upvote.style.color = "orange";
-    }
-    else{
-        currentUpvotes--;
-        totalUpvotes.innerHTML = `
-            ${currentUpvotes}
-        `;
-        setTimeout(() => {
-            totalUpvotes.style.color = "red";
-        }, 12);
-        setTimeout(() => {
-            totalUpvotes.style.color = "#1E40AF";
-        }, 1000);
-        upvote.style.color = "#6B7280";
-    }
-}
-function downvoteTick(){
-    isClicked = !isClicked;
-    if(isClicked){
-        currentUpvotes--;
-        totalUpvotes.innerHTML = `
-            ${currentUpvotes}
-        `;
-        setTimeout(() => {
-            totalUpvotes.style.color = "red";
-        }, 12);
-        setTimeout(() => {
-            totalUpvotes.style.color = "#1E40AF";
-        }, 1000);
-        downvote.style.color = "orange";
-    }
-    else{
-        currentUpvotes++;
-        totalUpvotes.innerHTML = `
-            ${currentUpvotes}
-        `;
-        setTimeout(() => {
-            totalUpvotes.style.color = "green";
-        }, 12);
-        setTimeout(() => {
-            totalUpvotes.style.color = "#1E40AF";
-        }, 1000);
-        downvote.style.color = "#6B7280";
-    }
-    
-}
 
 // CALLS
 // WHEN ADDING OTHER TWEETS PUT THE IMAGES AND TWEETS USERNAMES INTO THE ARRAY AND
 // ITERATE THROUGH THAT
 
-for(let i=0;i<5;i++){
+for(let i=0;i<3;i++){
     // TIMELINE CONTAINER
     const timeline = document.getElementById("timeline");
 
@@ -140,7 +58,7 @@ for(let i=0;i<5;i++){
     // USERNAME
     const username = document.createElement("div");
     username.id = "username";
-    username.className = "row-start-2 row-end-4 col-start-4 col-end-7 -ml-2 font-bold z-10 overflow";
+    username.className = "row-start-2 row-end-4 col-start-4 col-end-7 -ml-2 font-bold cursor-pointer hover:underline z-10 overflow";
     username.textContent = "amyrobson";
     tweet.appendChild(username);
 
@@ -163,20 +81,42 @@ for(let i=0;i<5;i++){
     reply.id = "reply";
     reply.className = "row-start-2 row-end-4 col-start-11 col-end-14 pr-2 text-right";
     tweet.appendChild(reply);
-    // REPLY BUTTON
+
+        // REPLY BUTTON
     const replyButton = document.createElement("button");
     replyButton.id = "reply-button";
     replyButton.className = "cursor-pointer pr-1";
     reply.appendChild(replyButton);
+
     const replyImg = document.createElement("img");
     replyImg.src = "images/icon-reply.svg";
     replyButton.appendChild(replyImg);
-    // REPLY SPAN
+
+        // REPLY SPAN
     const replyText = document.createElement("span");
     replyText.id = "reply-text";
     replyText.className = "font-bold text-blue-900";
     replyText.textContent = "Reply";
     reply.appendChild(replyText);
+
+    // DOM ELEMENTS
+
+    // EVENT LISTENERS
+    username.addEventListener("click", () => {
+        console.log("clicked username... visiting profile");
+    });
+    replyImg.addEventListener("click", () => {
+        console.log("clicked reply button");
+    });
+
+    // FUNCTIONS
+
+    // CALLS
+
+    // SANDBOX
+
+
+
 
 }
 
