1c65466b1c43c6f96165cb1117919b3c8e34a5f2
loganyeh <yehlogan@gmail.com>
2025-11-11 15:52:09 -0500

    time

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 
 // GLOBAL VARIABLES
 const timeline = document.getElementById("timeline");
+let newTweetCounter = 1;
+let tweetID = 0;
+
 const defaultUserProfiles = [
     {id: 1, username: "Nick", profilePicture: "images/avatars/image-juliusomo.png", datePosted: "3 years ago"},
     {id: 2, username: "Broussard", profilePicture: "images/avatars/image-amyrobson.png", datePosted: "2 years ago"},
-    {id: 3, username: "Wildes", profilePicture: "images/avatars/image-maxblagun.png", datePosted: "2 years ago"},
-    {id: 4, username: "Greg", profilePicture: "images/avatars/image-ramsesmiron.png", datePosted: "1 year ago"},
+    {id: 0, username: "Wildes", profilePicture: "images/avatars/image-maxblagun.png", datePosted: "2 years ago"},
+    {id: 0, username: "Greg", profilePicture: "images/avatars/image-ramsesmiron.png", datePosted: "1 year ago"},
 ];
 const newUserProfiles = [
     {id: 5, username: "Lamar", profilePicture: "images/avatars/image-amyrobson.png", datePosted: "Week 8"},
@@ -15,43 +18,34 @@ const newUserProfiles = [
 ];
 const tweetsArray = [
     () => createTweet(defaultUserProfiles[0].id, defaultUserProfiles[0].username, defaultUserProfiles[0].profilePicture, defaultUserProfiles[0].datePosted),
-    () => quoteTweet(defaultUserProfiles[1].id, defaultUserProfiles[1].username, defaultUserProfiles[1].profilePicture, defaultUserProfiles[1].datePosted),
+    // () => quoteTweet(defaultUserProfiles[1].id, defaultUserProfiles[1].username, defaultUserProfiles[1].profilePicture, defaultUserProfiles[1].datePosted),
     () => createTweet(defaultUserProfiles[2].id, defaultUserProfiles[2].username, defaultUserProfiles[2].profilePicture, defaultUserProfiles[2].datePosted),
-    () => quoteTweet(defaultUserProfiles[3].id, defaultUserProfiles[3].username, defaultUserProfiles[3].profilePicture, defaultUserProfiles[3].datePosted),
+    // () => quoteTweet(defaultUserProfiles[3].id, defaultUserProfiles[3].username, defaultUserProfiles[3].profilePicture, defaultUserProfiles[3].datePosted),
 ]
 tweetsArray.forEach((fn) => {
     fn();
 });
-let newTweetCounter = 4;
 
 // MAIN (REGULAR) FUNCTIONS
 function createTweet(profileTwitterID, profileUsername, profilePicture, datePosted){
     // TWEET CONTAINER ------------------------------------
     const tweet = document.createElement(`div`);
-    tweet.id = `Tweet#${profileTwitterID}`;
+    tweet.id = `Tweet#${tweetID}`;
     tweet.className = "h-44 w-5/6 grid grid-rows-13 grid-cols-13 mt-3 p-4 rounded-xl bg-white shadow-md";
     timeline.appendChild(tweet);
-                    
-    // UPVOTE/DOWNVOTE CONTAINER
-    addUpvotingSystem(tweet);
-    // PROFILE IMG
-    addProfilePicture(tweet, profilePicture);
-    // USERNAME
-    addUsername(tweet, profileUsername);
-    const username = addUsername(tweet, profileUsername);
-    usernameClickListener(username);
-    // DATE 
-    addDate(tweet, datePosted);
-    // TEXT 
-    addTwitterText(tweet);
+    tweet.textContent = `Tweet#: ${newTweetCounter}`;
+
     // REPLY 
     addReplyTextField(tweet);
     const replyButton = addReplyTextField(tweet);
     replyClickListener(replyButton, tweet.id, profileUsername, profilePicture);
 
-    console.log(`${tweet.id}`);
-
+    console.log(`TWEETID: ${tweet.id}`);
+    console.log(`TWEET#: ${newTweetCounter}`);
+    newTweetCounter++;
+    tweetID++;
 }
+
 function quoteTweet(profileTwitterID, profileUsername, profilePicture, datePosted){
     // QUOTE TWEET CONTAINER
     const quoteTweetContainer = document.createElement("div");
@@ -68,19 +62,7 @@ function quoteTweet(profileTwitterID, profileUsername, profilePicture, datePoste
     tweet.id = `tweet-${profileTwitterID}`;
     tweet.className = "h-full w-11/12 grid grid-rows-13 grid-cols-13 p-4 rounded-2xl bg-white shadow-md";
     quoteTweetContainerBorder.appendChild(tweet);
-    
-    // UPVOTE/DOWNVOTE CONTAINER
-    addUpvotingSystem(tweet);
-    // PROFILE IMG
-    addProfilePicture(tweet, profilePicture);
-    // USERNAME
-    addUsername(tweet, profileUsername);
-    const username = addUsername(tweet, profileUsername);
-    usernameClickListener(username);
-    // DATE 
-    addDate(tweet, datePosted);
-    // TEXT 
-    addTwitterText(tweet);
+
     // REPLY 
     addReplyTextField(tweet);
     const replyButton = addReplyTextField(tweet);
@@ -89,6 +71,7 @@ function quoteTweet(profileTwitterID, profileUsername, profilePicture, datePoste
     console.log(`${quoteTweetContainer.id}`);
     
 }
+
 function createReply(profileTwitterID, profileUsername, profilePicture, datePosted){
     // REPLY COMMENT CONTAINER -------------------------------
     const replyCommentContainer = document.createElement("div");
@@ -235,12 +218,21 @@ function usernameClickListener(username){
         console.log("clicked username... visiting profile");
     });
 }
-function replyClickListener(reply, id, profileUsername, profilePicture){
+function replyClickListener(reply, tweetID, profileUsername, profilePicture){
     reply.addEventListener("click", () => {
-        newTweetCounter++;
         console.log("reply button clicked");
-        console.log(`newTweetCoutner: ${newTweetCounter}`);
+        console.log(`newTweetCounter: ${newTweetCounter}`);
+        const insertNewTweetIndex = Number(tweetID.slice(6) - 1);
+
+        timeline.innerHTML =``;
+        tweetsArray.splice(insertNewTweetIndex, 0, createTweet(() => createTweet(defaultUserProfiles[0].id, defaultUserProfiles[0].username, defaultUserProfiles[0].profilePicture, defaultUserProfiles[0].datePosted),))
+        tweetsArray.forEach((fn) => {
+            fn();
+        });
+        // IT MIGHT SPLICE BUT THE REST OF THE OLD ARRAY DISSAPEARS ?? FIX ??
     });
 }
+
+
 
 
